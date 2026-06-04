ccbaecb8dfcbe59f25df78df08cfa4216db6bee6
Yarmola <sashayarmoluk07@gmail.com>
2026-06-04 21:57:43 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import { store } from '@/store';
@@ -22,11 +22,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter
-          future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-        >
-          <App />
-        </BrowserRouter>
+        <HashRouter
+              future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <App />
+        </HashRouter>
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
